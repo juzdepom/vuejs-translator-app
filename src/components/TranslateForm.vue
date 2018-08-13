9a4343d73bedd12d338637936acdf975e14f2400
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-08-12 20:31:53 -0400

    Initial commit. Already added some custom stuff.

---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="translateForm">
+        <form v-on:submit="formSubmit">
+            <input type="text" v-model="textToTranslate" placeholder="Enter a word">
+            <input type="submit" value="Translate">
+        </form>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'translateForm',
+    data(){
+        return {
+            textToTranslate: '',
+        }
+    },
+    methods: {
+        formSubmit(e){
+            alert('Submitted');
+            e.preventDefault(); //prevents the default submit
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
